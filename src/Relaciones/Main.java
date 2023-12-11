12c5d4faf1503c86ab5c1ff5a19807a76a6a850c
Andres Diaz Carrillo <96279922+AndresDiazCarrillo@users.noreply.github.com>
2023-12-11 17:24:05 +0100

    Clase grafo añadida

--- src/Relaciones/Main.java
+++ src/Relaciones/Main.java
@@ -1,5 +1,41 @@
 package Relaciones;
 
+import java.io.File;
+import java.util.Scanner;
+import graphsDSESIUCLM.*;
+
 public class Main {
 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("¿Que fichero quieres leer?");
+	    String ficherito = scanner.next();
+		
+		
+		File fichero = new File(ficherito);
+		Scanner s = null;
+	
+		try {
+			// Leemos el contenido del fichero
+			System.out.println("... Leemos el contenido del fichero ...");
+			s = new Scanner(fichero);
+	
+			// Leemos linea a linea el fichero
+			while (s.hasNextLine()) {
+				String linea = s.nextLine(); 	// Guardamos la linea en un String
+				System.out.println(linea);      // Imprimimos la linea
+			}
+	
+		} catch (Exception ex) {
+			System.out.println("Mensaje: " + ex.getMessage());
+		} finally {
+			// Cerramos el fichero tanto si la lectura ha sido correcta o no
+			try {
+				if (s != null)
+					s.close();
+			} catch (Exception ex2) {
+				System.out.println("Mensaje 2: " + ex2.getMessage());
+			}
+		}
+	}
 }
